234b940f7dc5f69ea4b750331803437a68266529
Avles1234 <selenagomezdalapa@email.com>
2023-08-04 17:25:42 -0300

    jota

---
+++ src/components/Home/index.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,FlatList } from 'react-native';
+import BannerFilmes from './src/components/bannerFilmes';
+import Filmes from './src/data/movies'
+import Header from './src/components/header';
+import SearchBar from './src/components/searchbar';
+import CardMovies from './src/components/cardMovies';
+
+
+export default function Details(){
+    return(
+
+            <View style=(styles.container)>
+                <View ESSA PÁGINA DE DETALHES Text> 
+            /View>
+
+
+
+
+    )
+
+
+
+
+
+}
---
+++ src/data/filmes.js
@@ -0,0 +1,117 @@
+const DATA = 
+[
+{
+    id:'01',
+    nome:'Camisa Retrô 13/14 AC Milan',
+    preço:'R$389,99',
+    modelo:'CamisaRetrô1314ACMilan.png',
+    tamanho: 'P ao GG',
+    cor: 'Dourada'
+},
+{
+    id:'02',
+    nome:'Camisa Retrô 15/16 Barcelona',
+    preço:'R$479,99',
+    modelo:'CamisaRetrô1516Barcelonahome.png',
+    tamanho: 'P ao GG',
+    cor: 'Clássica'
+},
+{
+    id:'03',
+    nome:'Camisa Retrô 2014 Alemanha',
+    preço:'R$499.99',
+    modelo:'CamisaRetro2014Alemanhahome.png',
+    tamanho: 'P ao GG',
+    cor: 'Branca'
+},
+{
+    id:'04',
+    nome:'Camisa Retrô 2006 Portugal',
+    preço:'R$299,99',
+    modelo:'CamisaRetroPortugal2006home.png',
+    tamanho: 'P ao GG',
+    cor: 'Vermelha'
+},
+{
+    id:'05',
+    nome:'Camisa Retrô 2000 Vasco da Gama',
+    preço:'R$179,99',
+    modelo:'CamisaRetroVascodaGama2000.png',
+    tamanho: 'P ao GG',
+    cor: 'Branca'
+},
+{
+    id:'06',
+    nome:'Camisa Retrô 93/94 Palmeiras home',
+    preço:'R$129,90',
+    modelo:'Retro9293Palmeirashome.png',
+    tamanho: 'P ao GG',
+    cor: 'Verde'
+},
+{
+    id:'07',
+    nome:'Camisa Retrô Chelsea 08/09',
+    preço:'R$219,99',
+    modelo:'Retro9293Chelseahome.png',
+    tamanho: 'P ao GG',
+    cor: 'Azul'
+},
+{
+    id:'08',
+    nome:'Camisa Retrô Corinthians 2012',
+    preço:'R$299,89',
+    modelo:'RetroCorinthians2012Home.png',
+    tamanho: 'P ao GG',
+    cor: 'Branca'
+},
+{
+    id:'09',
+    nome:'Tracksuit Barcelona',
+    preço:'R$285,99',
+    modelo:'TracksuitBarcelona.png',
+    tamanho: 'P ao GG',
+    cor: 'Preto'
+},
+{
+    id:'10',
+    nome:'Tracksuit Espanha',
+    preço:'R$379,99',
+    modelo:'TracksuitEspanha.png',
+    tamanho: 'P ao GG',
+    cor: 'Vermelha'
+},
+{
+    id:'11',
+    nome:'Tracksuit Portugal',
+    preço:'R$359,99',
+    modelo:'TracksuitPortugal.png',
+    tamanho: 'P ao GG',
+    cor: 'Azul Marinho'
+},
+{
+    id:'12',
+    nome:'Tracksuit PSG',
+    preço:'R$229,99',
+    modelo:'TracksuitPSG.png',
+    tamanho: 'P ao GG',
+    cor: 'Branca'
+},
+{
+    id:'13',
+    nome:'Tracksuit São Paulo',
+    preço:'R$209,99',
+    modelo:'TracksuitSãoPaulo.png',
+    tamanho: 'P ao GG',
+    cor: 'Preta'
+},
+{
+    id:'14',
+    nome:'Tracksuit Vasco da Gama',
+    preço:'R$248,85',
+    modelo:'TracksuitVascoDaGama.png',
+    tamanho: 'P ao GG',
+    cor: 'Preta'
+},
+]
+
+export default DATA;
