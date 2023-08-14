99d6f91d31a9ad4b1865ccb54ecb81e670ea28f3
joao <selenagomezdalapa@gmail.com>
2023-08-14 18:07:49 -0300

    Mudanças

---
+++ src/data/filmes.js
@@ -0,0 +1,109 @@
+const DATA = 
+[
+{
+    id:'01',
+    nome:'Camisa Retrô 13/14 AC Milan',
+    preço:'R$389,99',
+    modelo:'CamisaRetrô1314ACMilan.png',
+    tamanho: 'P ao GG',
+    cor: 'Dourada'
+},
+{
+    id:'02',
+    nome:'Camisa Retrô 15/16 Barcelona',
+    preço:'R$479,99',
+    modelo:'CamisaRetrô1516Barcelonahome.png',
+    tamanho: 'P ao GG',
+    cor: 'Clássica'
+},
+{
+    id:'03',
+    nome:'Camisa Retrô 2014 Alemanha',
+    preço:'R$499.99',
+    modelo:'CamisaRetro2014Alemanhahome.png',
+    tamanho: 'P ao GG',
+    cor: 'Branca'
+},
+{
+    id:'04',
+    nome:'Camisa Retrô 2006 Portugal',
+    preço:'R$299,99',
+    modelo:'CamisaRetroPortugal2006home.png',
+    tamanho: 'P ao GG',
+    cor: 'Vermelha'
+},
+{
+    id:'05',
+    nome:'Camisa Retrô 2000 Vasco da Gama',
+    preço:'R$179,99',
+    modelo:'CamisaRetroVascodaGama2000.png',
+    tamanho: 'P ao GG',
+    cor: 'Branca'
+},
+{
+    id:'06',
+    nome:'Camisa Retrô Chelsea 08/09',
+    preço:'R$475,89',
+    modelo:'RetroChelsea0809home.png',
+    tamanho: 'P ao GG',
+    cor: 'Azul'
+},
+{
+    id:'07',
+    nome:'Camisa Retrô ',
+    preço:'R$389,99',
+    modelo:'CamisaRetrô1314ACMilan.png',
+    tamanho: 'P ao GG',
+    cor: 'Dourada'
+},
+{
+    id:'08',
+    nome:'Camisa Retrô AC Milan',
+    preço:'R$389,99',
+    modelo:'CamisaRetrô1314ACMilan.png',
+    tamanho: 'P ao GG',
+    cor: 'Dourada'
+},
+{
+    id:'09',
+    nome:'Camisa Retrô AC Milan',
+    preço:'R$389,99',
+    modelo:'CamisaRetrô1314ACMilan.png',
+    tamanho: 'P ao GG',
+    cor: 'Dourada'
+},
+{
+    id:'10',
+    nome:'Camisa Retrô AC Milan',
+    preço:'R$389,99',
+    modelo:'CamisaRetrô1314ACMilan.png',
+    tamanho: 'P ao GG',
+    cor: 'Dourada'
+},
+{
+    id:'11',
+    nome:'Camisa Retrô AC Milan',
+    preço:'R$389,99',
+    modelo:'CamisaRetrô1314ACMilan.png',
+    tamanho: 'P ao GG',
+    cor: 'Dourada'
+},
+{
+    id:'12',
+    nome:'Camisa Retrô AC Milan',
+    preço:'R$389,99',
+    modelo:'CamisaRetrô1314ACMilan.png',
+    tamanho: 'P ao GG',
+    cor: 'Dourada'
+},
+{
+    id:'13',
+    nome:'Camisa Retrô AC Milan',
+    preço:'R$389,99',
+    modelo:'CamisaRetrô1314ACMilan.png',
+    tamanho: 'P ao GG',
+    cor: 'Dourada'
+},
+]
+
+export default DATA;
